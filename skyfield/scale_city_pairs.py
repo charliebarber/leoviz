f6dce9edbad44274b2442d0cf7dd544b665a590c
charliebarber <charlieb@protonmail.com>
2025-01-28 20:58:30 +0000

    Scaled city code

---
+++ skyfield/scale_city_pairs.py
@@ -0,0 +1,54 @@
+from ground_stations import GroundStations
+import math
+import csv
+
+def haversine_distance(gs1, gs2):
+    # Radius of the Earth in kilometers
+    R = 6371.0
+
+    # Convert degrees to radians
+    lat1 = math.radians(gs1['latitude'])
+    lon1 = math.radians(gs1['longitude'])
+    lat2 = math.radians(gs2['latitude'])
+    lon2 = math.radians(gs2['longitude'])
+
+    # Differences in coordinates
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+
+    # Haversine formula
+    a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+    # Distance in kilometers
+    distance = R * c
+    return distance
+
+ground_stations = GroundStations("./cities.csv")
+
+# gs_pairs = [(gs1, gs2) for i, gs1 in enumerate(ground_stations) 
+# #                    for gs2 in ground_stations[i+1:]]
+
+# print(gs_pairs)
+gs_pos = ground_stations.get_station_positions()
+
+gs_pairs = [(gs1, gs2) for i, gs1 in enumerate(gs_pos) for gs2 in gs_pos[i+1:]]
+
+max_pop_product = gs_pos[0]['population'] * gs_pos[1]['population']
+max_dist = max([haversine_distance(gs1, gs2) for i, gs1 in enumerate(gs_pos) for gs2 in gs_pos[i+1:]])
+# print(max_pop_product)
+# print(max_dist)
+
+data = [["gs1", "gs2", "scaled_pop_product", "scaled_dist"]]
+
+for gs1, gs2 in gs_pairs:
+  pop_product = gs1['population'] * gs2['population']
+  scaled_pop_product = pop_product / max_pop_product
+  dist = haversine_distance(gs1, gs2)
+  scaled_dist = dist / max_dist
+  # print(f"{gs1['name']} {gs2['name']} {scaled_pop_product} {scaled_dist}")
+  data.append([gs1['id'], gs2['id'], scaled_pop_product, scaled_dist])
+
+with open('cities_scaled.csv', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(data)
